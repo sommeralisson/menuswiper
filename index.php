2fba50e0d23c2c0106968f900dfe3dfb7e07f884
Alisson Sommer <alissonsommer1@gmail.com>
2022-06-13 21:00:52 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>ÁREA CENTRAL [ ÁREA CENTRAL ]</title>
+
+	<link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+	<div class="container">
+		<main>
+			<div class="header">
+				<p>Precisa de ajuda?</p>
+				<button class="suporte">Suporte</button>
+			</div>
+			<div class="instrucoes">
+				<div class="texto">
+					<h2>
+						Instruções
+					</h2>
+					<ul>
+						<li>Se você for administrador, associado ou fornecedor identifique-se ao lado, informando seu usuário e senha;</li>
+						<li>Sua senha é de uso exclusivo, não divulgue a ninguém; Não esqueça de sair do sistema após o uso;</li>
+						<li>Se você não conseguir permanecer conectado ao sistema, verifique a configuração de cookies de seu navegador.</li>
+					</ul>
+				</div>
+				<div class="rodape-icon">
+					<img src="./img/logo-AC.png" alt="">
+				</div>
+			</div>
+			<div class="principal">
+				<div class="login">
+					<img src="https://demo.areacentral.com.br/401/empresas/demo/logo-demo.png" alt="">
+					<h3>Entrar</h3>
+					<p>Informe seu usuário e senha de acesso abaixo</p>
+					<label for="usuario">
+						Usuário
+					</label>
+					<br>
+					<input type="text" placeholder="Informe seu usuário">
+					<br>
+					<label for="senha">
+						Senha
+					</label>
+					<br>
+					<input type="password" placeholder="Informe sua senha">
+					<button class="entrar">Entrar</button>
+				</div>
+			</div>
+		</main>
+	</div>
+</body>
+</html>
